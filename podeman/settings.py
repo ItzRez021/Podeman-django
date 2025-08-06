bfd75a9d87c0b68f077ae8c41997e2834e1cf38f
WEBRAH <87922132+ItzRez021@users.noreply.github.com>
2025-08-06 23:34:51 +0330

    Update settings.py

--- podeman/settings.py
+++ podeman/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&%qm+#-*v5+1ro^#riu3x2e7-f6_j&av$tf^v6s&na=je2jttw'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
